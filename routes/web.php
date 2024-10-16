e66f3b04630f7c32c51abca8506b6bd6b41cb4a8
FalenElora <faleneloranuro@gmail.com>
2024-10-16 14:14:30 +0700

    add index and login page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); 
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); //merujuk ke file welcome di folder resources
+});
+
+Route::get('/login', function () {
+    return view('login'); 
 });
